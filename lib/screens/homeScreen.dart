8406d63a88a9cd5b8f30867239e0067a4982d8ee
Aditya Sharma <87749440+adityaSharmaWritesCode@users.noreply.github.com>
2022-07-22 19:53:02 +0530

    Update homeScreen.dart

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,3 +1,4 @@
+import 'package:dash_call/widgets/home_button.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,11 +10,94 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _page = 0;
+  onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text('Meet & Chat'),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).bottomAppBarColor,
+        selectedItemColor: Theme.of(context).highlightColor,
+        unselectedItemColor: Colors.grey,
+        onTap: onPageChanged,
+        currentIndex: _page,
+        type: BottomNavigationBarType.fixed,
+        unselectedFontSize: 14.0,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.comment_bank),
+            label: 'Meet & Chat',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.lock_clock),
+            label: 'Meetings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: 'Contacts',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings_outlined),
+            label: 'Settings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.comment_bank),
+            label: 'Meet & Chat',
+          ),
+        ],
+      ),
       body: SafeArea(
-        child: Container(),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                HomeButton(
+                  icon: Icons.video_call,
+                  text: 'New Meeting',
+                  onPressed: () {},
+                ),
+                HomeButton(
+                  icon: Icons.add_box_rounded,
+                  text: 'Join Meeting',
+                  onPressed: () {},
+                ),
+                HomeButton(
+                  icon: Icons.calendar_today,
+                  text: 'Schedule',
+                  onPressed: () {},
+                ),
+                HomeButton(
+                  icon: Icons.arrow_upward_rounded,
+                  text: 'Share Screen',
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            const Expanded(
+              child: Center(
+                child: Text(
+                  'Create or Join Meetings with just a taps!',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
