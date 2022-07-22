c85b168230e09822dc1930f070fd0af2a116781b
Aditya Sharma <87749440+adityaSharmaWritesCode@users.noreply.github.com>
2022-07-22 19:52:21 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:dash_call/resources/auth_methods.dart';
 import 'package:dash_call/screens/homeScreen.dart';
 import 'package:dash_call/screens/loginScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -21,12 +25,29 @@ class MyApp extends StatelessWidget {
             const Color.fromRGBO(46, 46, 46, 1), // SECONDARY BACKGROUND COLOR
         primaryColor: const Color.fromRGBO(14, 114, 236, 1), // BUTTON COLOR
         bottomAppBarColor: const Color.fromRGBO(26, 26, 26, 1), // FOOTER COLOR
+        highlightColor: Colors.white, //SELECTED ITEM COLOR
       ),
       routes: {
         LoginScreen.route: (context) => const LoginScreen(),
         HomeScreen.route: (context) => const HomeScreen(),
       },
-      initialRoute: LoginScreen.route,
+      // initialRoute: HomeScreen.route,
+      home: StreamBuilder(
+        stream: AuthMethods().authChanges,
+        builder: ((context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapshot.hasData) {
+            return const HomeScreen();
+          }
+
+          return const LoginScreen();
+        }),
+      ),
     );
   }
 }
